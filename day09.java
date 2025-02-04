100678b03cc0e04665b81156dca2b27e382fc1b4
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-04 12:57:47 +0530

    day9

---
+++ day09.java
@@ -0,0 +1,49 @@
+//leetcode problem no. 23 -------- (Merge k sorted Lists)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode result = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            result = mergeList(result, lists[i]);
+        }
+        return result;
+    }
+
+    public ListNode mergeList(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(0);
+        ListNode current = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if (list1 != null) {
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
+        }
+        return temp.next;
+    }
+}
