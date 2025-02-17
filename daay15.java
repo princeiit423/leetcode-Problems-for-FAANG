361d8aff26e0769ab96068bd3cd2a8bed3d75646
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-17 20:52:38 +0530

    added

---
+++ daay15.java
@@ -0,0 +1,17 @@
+//leetcode problem no.(53) ------------ maximum subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = currentSum + nums[i];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
