e0f639f24df84dce77b19af92b7c2ba9ac0775dd
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-21 12:23:34 +0530

    day17 added

---
+++ day17.java
@@ -0,0 +1,14 @@
+// leetcode problem number (26) -- Remove duplicate from sorted arrays
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
